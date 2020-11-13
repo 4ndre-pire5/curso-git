3018b3b33d5ee4ec15377d511eee5f99312b989d
Andre Pires <pirao@uol.com.br>
2020-11-13 15:13:54 -0300

    Acrescentada menagem de Bom dia.

--- ProjGit/Program.cs
+++ ProjGit/Program.cs
@@ -4,6 +4,7 @@ namespace ProjGit {
     class Program {
         static void Main(string[] args) {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Bom dia!");
         }
     }
 }
